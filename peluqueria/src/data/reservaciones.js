21d9645ee672cb36e45a989903b35f79b95f0026
mialvera <miltonvera96@gmail.com>
2017-12-10 20:52:24 -0500

    res

---
+++ peluqueria/src/data/reservaciones.js
@@ -0,0 +1,12 @@
+{
+	"reservacion1": {"fecha":"15-12-2017","hora":"10:00", "cliente":"Elena Moran","empleado":"Adriana Sevillano"},
+	"reservacion2": {"fecha":"15-12-2017","hora":"11:00", "cliente":"Mariela Robayo","empleado":"Elena Sanchez"},
+	"reservacion3": {"fecha":"15-12-2017","hora":"12:00", "cliente":"Kevin Lopez","empleado":"Veronica Sanchez"},
+	"reservacion4": {"fecha":"15-12-2017","hora":"14:00", "cliente":"Ivan Robayo","empleado":"Mariela Perez"},
+	"reservacion5": {"fecha":"15-12-2017","hora":"15:00", "cliente":"Kevin Oyola","empleado":"Daniela Carrazco"},
+	"reservacion6": {"fecha":"16-12-2017","hora":"09:00", "cliente":"Marcelo Robayo","empleado":"Daira Robayo"},
+	"reservacion7": {"fecha":"16-12-2017","hora":"10:00", "cliente":"Carolina del Salto","empleado":"Adriana Sevillano"},
+	"reservacion8": {"fecha":"16-12-2017","hora":"11:00", "cliente":"Michelle Toure","empleado":"Elena Sanchez"},
+	"reservacion9": {"fecha":"16-12-2017","hora":"12:00", "cliente":"Sandra Angulo","empleado":"Adriana Sevillano"},
+	"reservacion10": {"fecha":"16-12-2017","hora":"14:00", "cliente":"Veronica Urgiles","empleado":"Estefania Yanez"}
+}
